af90a885a67311afa2163a085650d4c554e9c45d
ArwinW <239881@edu.rocfriesepoort.nl>
2021-11-15 17:41:04 +0100

    klant toevoegen

--- klantenoverzicht.php
+++ klantenoverzicht.php
@@ -1,6 +1,7 @@
 <?php
 include "config.php";
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,62 +12,102 @@ include "config.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KlantenOverzicht</title>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    
+    <?php
+    $error_message = "";$success_message = "";
 
+    if(isset($_POST['btnsignup'])){
+        $factuuradres = trim($_POST['Factuuradres']);
+        $postcode = trim($_POST['Postcode']);
+        $tel = trim($_POST['Tel']);
+        $email = trim($_POST['Email']);
+        $adres = trim($_POST['Adres']);
+        $bedrijfsnaam = trim($_POST['Bedrijfsnaam']);
+        $wachtwoord = trim($_POST['Wachtwoord']);
+
+        $isValid = true;
+
+        if($factuuradres == ''|| $postcode == '' || $tel == '' || $email == '' || $adres == '' || $bedrijfsnaam == '' || $wachtwoord == ''){
+        $isValid = false;
+        $error_message = "Vul alle velden in.";
+        }
+
+        if ($isValid && !filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $isValid = false;
+            $error_message = "E-mail is niet geldig.";
+        }
+
+        if($isValid){
+
+            $stmt = $con->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt -> bind_param("s", $email);
+            $stmt -> execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+            if($result->num_rows > 0){
+                $isValid - false;
+                $error_message = "Email bestaat al";
+            }
+        }
+
+        if($isValid){
+            $insertSQL = "INSERT INTO users(Factuuradres,Postcode,Tel,Email,Adres,Naam,Wachtwoord ) values(?,?,?,?,?,?,?)";
+            $stmt = $con-> prepare($insertSQL);
+            $stmt->bind_param("sssssss", $factuuradres,$postcode,$tel,$email,$adres,$bedrijfsnaam,$wachtwoord);
+            $stmt -> execute();
+            $stmt -> close();
+            
+            $success_message = "Account aangemaakt.";
+        }
+
+    }
+    ?>
 </head>
 <body>
-<div class="container">
-<div class="row">
-<div class="blok">
-    <div class="col-md-6">
-    <div>
 
     <form method="post" action="">
-
-         <h2 class="bedrijf_gegevens">Bedrijf Gegevens</h2><br>  
-         <?php 
-         if(!empty($error_message)){
-         ?>
-         <div class="alert alert-danger">
-             <strong>Error!</strong> <?= $error_message ?>
-         </div>
-        <?php
-        }
-        ?>
-
         <?php 
-         if(!empty($success_message)){
-         ?>
-         <div class="alert alert-danger">
-             <strong>Success!</strong> <?= $success_message ?>
-         </div>
-        <?php
-        }
+        if(!empty($error_message)){
         ?>
-
-        <div class="form-group">
-        <label for='Factuuradres'></label>
-        <input type="text" class="form-control" name="factuuradres" id=factuuradres placeholder="Factuuradres" required="required">
+        <div class="alert alert-danger">
+            <strong>Error!</strong> <?= $error_message ?>
         </div>
-        <input type="text" class="form-control" placeholder="Postcode"><br><br>
-        <input type="text" class="form-control" placeholder="Telefoon nummer"><br><br>
-        <input type="text" class="form-control" placeholder="E-mail"><br><br>
-        <input type="text" class="form-control" placeholder="Adres"><br><br>
+       <?php
+       }
+       ?>
+
+       <?php 
+        if(!empty($success_message)){
+        ?>
+        <div class="alert alert-danger">
+            <strong>Success!</strong> <?= $success_message ?>
+        </div>
+       <?php
+       }
+       ?>
+
+<div class="blok">
+    <div>
+         <h2 class="bedrijf_gegevens">Bedrijf Gegevens</h2><br>  
+        <input type="text" name="Factuuradres" class="bedrijfsnaam" placeholder="Factuuradres"><br><br>
+        <input type="text" name="Postcode" class="bedrijfsnaam" placeholder="Postcode"><br><br>
+        <input type="text" name="Tel" class="bedrijfsnaam" placeholder="Telefoon nummer"><br><br>
+        <input type="text" name="Email" class="bedrijfsnaam" placeholder="E-mail"><br><br>
+        <input type="text" name="Adres" class="bedrijfsnaam" placeholder="Adres"><br><br>
    
       
     <h2 class="inlog_gegevens">Inlog Gegevens</h2><br>
-        <input type="text" class="form-control" placeholder="Bedrijfsnaam"><br><br>
-        <input type="text" class="form-control" placeholder="Wachtwoord"><br><br>
+        <input type="text" name="Bedrijfsnaam" class="bedrijfsnaam" placeholder="Bedrijfsnaam"><br><br>
+        <input type="password" name="Wachtwoord" class="password" placeholder="Wachtwoord"><br><br>
             <h4 class="wachtwoord_eisen">- Hoofdletter</h4>
             <h4 class="wachtwoord_eisen">- Speciaal teken</h4>
             <h4 class="wachtwoord_eisen">- 8 Caracters</h4><br><br><br>
-            <button class="klant-toevoegen">Voeg toe</button>
+            <button type="submit" name="btnsignup" class="klant-toevoegen">Voeg toe</button>
     </div>
 </div>
-</div>
-</div>
-</div>
+
 </body>
 </html>
