eba85a1f45410641440c392afb9d0d7bf852932f
Timhamer <240159@edu.rocfriesepoort.nl>
2021-11-16 13:27:27 +0100

    d

--- Bestel.php
+++ Bestel.php
@@ -9,13 +9,11 @@
         die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error );
     }
     
-    $sql = "INSERT INTO `bestelformulier`(`Aantal`, `Voornaam`, `E-mail`, `Tussenvoegsel`, `Adres`, `Achternaam`, `Postcode`, `Telefoonnummer`, `Status`) VALUES (?,?,?,?,?,?,?,?,?)";
+    $sql = "INSERT INTO `bestelformulier`(`Aantal`, `Voornaam`, `E-mail`, `Tussenvoegsel`, `Adres`, `Achternaam`, `Postcode`, `Telefoonnummer`) VALUES (?,?,?,?,?,?,?,?)";
     $_POST['Aantal'] ?? 0;
-    $_POST['Status'] ?? 0;
-
 
     $insert = $mysqli->prepare($sql);
-    $insert->bind_param('isssssss', $_POST['Aantal'], $_POST['Voornaam'], $_POST['E-mail'], $_POST['Tussenvoegsel'], $_POST['Adres'], $_POST['Achternaam'], $_POST['Postcode'], $_POST['Telefoonnummer'], $_POST['Status']);
+    $insert->bind_param('isssssss', $_POST['Aantal'], $_POST['Voornaam'], $_POST['E-mail'], $_POST['Tussenvoegsel'], $_POST['Adres'], $_POST['Achternaam'], $_POST['Postcode'], $_POST['Telefoonnummer']);
     
     
     if ($insert->execute())  {
