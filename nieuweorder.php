42c1399242501b5de96fc8e6951ba695f40be596
Timhamer <240159@edu.rocfriesepoort.nl>
2021-11-17 12:07:07 +0100

    17-11-2021

--- nieuweorder.php
+++ nieuweorder.php
@@ -1,5 +1,3 @@
-
-
 <?php
 if(!$_POST){
   (header("location: bestel.html"));
@@ -11,16 +9,22 @@ if(!$_POST){
     }
     
     $sql = "INSERT INTO `bestelformulier`(`Aantal`, `Voornaam`, `E-mail`, `Tussenvoegsel`, `Adres`, `Achternaam`, `Postcode`, `Telefoonnummer`) VALUES (?,?,?,?,?,?,?,?)";
-    $_POST['Aantal'] ?? 0;
-
+    $_POST['Voornaam'] ?? 0;
+    $_POST['E-mail'] ?? 0;
+    $_POST['Tussenvoegsel'] ?? 0;
+    $_POST['Achternaam'] ?? 0;
+    $_POST['Postcode'] ?? 0;
+    $_POST['Telefoonnummer'] ?? 0;
+    echo $sql;
+    
     $insert = $mysqli->prepare($sql);
-    $insert->bind_param('isssssss', $_POST['Aantal'], $_POST['Voornaam'], $_POST['E-mail'], $_POST['Tussenvoegsel'], $_POST['Adres'], $_POST['Achternaam'], $_POST['Postcode'], $_POST['Telefoonnummer']);
+    $insert->bind_param('ssssssss', $_POST['Aantalklant'], $_POST['Voornaam'], $_POST['E-mail'], $_POST['Tussenvoegsel'], $_POST['Adresklant'], $_POST['Achternaam'], $_POST['Postcode'], $_POST['Telefoonnummer']);
     
     
     if ($insert->execute())  {
         echo "
           <center>
-       S     <body style='font-size:x-large;'>
+            <body style='font-size:x-large;'>
            Uw bestelling is succesvol verstuurd.
             <br>
             <strong>Bedankt voor uw bestelling<strong>
