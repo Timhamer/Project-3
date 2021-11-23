4579096a044af17fdd048e427e6aa3686dd99b51
ArwinW <89460606+ArwinW@users.noreply.github.com>
2021-11-23 20:39:15 +0100

    Update home.php

--- home.php
+++ home.php
@@ -17,19 +17,6 @@ if(!isset($_SESSION['id'])){
 if(isset($_POST['but_logout'])){
     session_destroy();
     header('Location: index.php');
-
+}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-    <body>
-        <h1>homepage</h1>
-       
-</body>
-</html>
