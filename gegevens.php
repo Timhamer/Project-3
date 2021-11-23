b2f4d31d65a2350e66cfab4a3f624cd59bd1cb43
ArwinW <239881@edu.rocfriesepoort.nl>
2021-11-23 13:45:10 +0100

    Update gegevens.php

--- gegevens.php
+++ gegevens.php
@@ -1,5 +1,6 @@
 <?php 
  include "config.php";
+ include "header.html";
  $_ACCOUNTID = $_SESSION['id'];
 
 $sql = mysqli_query($con, "select * from users where id='$_ACCOUNTID'");
@@ -22,18 +23,18 @@ $row = mysqli_fetch_array($sql)
     <div class="gegevens">
         <h2 class="titelgegevens">Gebruikers gegevens</h2>
         <h2 class="gegevensnaam">Naam:</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Naam'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Naam']; ?></h2>
+        <h2 class="data" contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Naam'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Naam']; ?></h2>
         <h2 class="gegevensnaam">Wachtwoord:</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Wachtwoord'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Wachtwoord']; ?></h2>
+        <h2 class="data"contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Wachtwoord'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Wachtwoord']; ?></h2>
         <h2 class="gegevensnaam">Factuuradres:</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Factuuradres'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Factuuradres']; ?></h2>
-        <h2 class="gegevensnaam">Adres</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Adres'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Adres']; ?></h2>
-        <h2 class="gegevensnaam">Postcode</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Postcode'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Postcode']; ?></h2>
-        <h2 class="gegevensnaam">Telefoonnummer</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Tel'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Tel']; ?></h2>
-        <h2 class="gegevensnaam">E-mail</h2>
-        <h2 contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Email'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Email']; ?></h2>
+        <h2 class="data" contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Factuuradres'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Factuuradres']; ?></h2>
+        <h2 class="gegevensnaam">Adres:</h2>
+        <h2 class="data" contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Adres'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Adres']; ?></h2>
+        <h2 class="gegevensnaam">Postcode:</h2>
+        <h2 class="data" contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Postcode'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Postcode']; ?></h2>
+        <h2 class="gegevensnaam">Telefoonnummer:</h2>
+        <h2 class="data" contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Tel'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Tel']; ?></h2>
+        <h2 class="gegevensnaam">E-mail:</h2>
+        <h2 class="data" contenteditable="true" onkeydown="return (event.keyCode!=13);" data-old_value="><?php echo $row['Email'];?> "onBlur="saveInlineEdit(this,'Naam', '<?php echo $row['id']; ?> ')" onClick="highlightEdit(this);"><?php echo $row['Email']; ?></h2>
     </div>
 </body>
