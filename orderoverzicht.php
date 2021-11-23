ecfa2cac6d863f4b83a07b6dce2dee868cbf39da
Timhamer <240159@edu.rocfriesepoort.nl>
2021-11-23 14:30:30 +0100

    Update orderoverzicht.php

--- orderoverzicht.php
+++ orderoverzicht.php
@@ -1,7 +1,20 @@
 <?php
 include "config.php";
 $_ACCOUNTID = $_SESSION['id'];
-
+?>
+<table class="content-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Naam</th>
+            <th>Adres</th>
+            <th>Factuuradres</th>
+            <th>E-mail</th>
+            <th>Telefoonnummer</th>
+            
+          </tr>
+        </thead>
+<?php
 $mysqli = new mysqli("localhost","root","","biermanagement");
 
 if ($mysqli -> connect_errno) {
@@ -9,7 +22,45 @@ if ($mysqli -> connect_errno) {
   exit();
 }
 
-$SQL = "SELECT * FROM users WHERE id='$_ACCOUNTID'"
-echo $_ACCOUNTID
 
+$sql = "SELECT * FROM users WHERE id = $_ACCOUNTID";
+ 
+  if ($result = $mysqli->query($sql)) {
+    foreach ($result as $row) {
+    echo "
+    <tr>
+        <td>" . $row['id'] . "</td> 
+        <td>" . $row['Naam']  . "</td>
+        <td> " . $row['Adres'] . $row['Postcode'] . "</td>
+        <td> " . $row['Factuuradres'] . "</td>
+        <td>". $row['Email'] .  "</td>
+        <td> " . $row['Tel'] . "</td>";
+ 
+         echo "";
+      
+ 
+      
+  }
+  $result -> free_result();
+}
+
+
+$mysqli -> close();
+
+  if(isset($_GET))
 ?>
+<!DOCTYPE html>
+<head>
+  <title>overzicht begheerder</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+   <link rel="stylesheet" href="header.css">
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="overzicht.js">'
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+</head>
+
+<body> 
+  
+</body>
+</html>
